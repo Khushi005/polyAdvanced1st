e9d29b8e49314bc9c336795045d1c2678dda9df3
Khushi005 <132692713+Khushi005@users.noreply.github.com>
2024-09-30 21:02:29 +0530

    Create batchMint.js

---
+++ scripts/batchMint.js
@@ -0,0 +1,40 @@
+// This script batch mints mylili ERC721A tokens.
+
+// Import required libraries
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  // Get private key from env
+  const privateKey = process.env.PRIVATE_KEY;
+
+  // The URL of the network provider
+  const networkAddress =
+    "https://ethereum-sepolia-rpc.publicnode.com";
+
+  // Create a provider using the URL
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+
+  // Create a signer from the private key and provider
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  // Tthe address of the deployed contract
+  const contractAddress = "0xa0B68e4A2d91FEee686663B4ad2c7b2dBe0f42B7";
+
+  // Get the contract factory and attach it to the signer
+  const IndianNFT = await ethers.getContractFactory("mylili", signer);
+  const contract = await IndianNFT.attach(contractAddress);
+
+  // Call the mint function on the contract to mint 5 tokens
+  await contract.mint(5);
+
+  // Log a message to the console to indicate that the tokens have been minted
+  console.log("Minted 5 tokens");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
