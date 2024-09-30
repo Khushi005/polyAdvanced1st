0030314128ec6d4e2b3c96f58562b1d672173116
Khushi005 <132692713+Khushi005@users.noreply.github.com>
2024-09-30 20:32:43 +0530

    Create package.config.js

---
+++ package.config.js
@@ -0,0 +1,13 @@
+{
+  "name": "hardhat-project",
+  "version": "1.0.0",
+  "lockfileVersion": 2,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "hardhat-project",
+      "dependencies": {
+        "@maticnetwork/meta": "^2.4.54",
+…    }
+  }
+}
