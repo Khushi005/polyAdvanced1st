1c272984be9e4911312305a5d46ab8e38603ecf1
Khushi005 <132692713+Khushi005@users.noreply.github.com>
2024-09-30 20:31:56 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+module.exports = {
+  solidity: "0.8.18",
+};
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+…},
+
+}
+};
